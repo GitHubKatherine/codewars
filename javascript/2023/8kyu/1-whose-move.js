e6b0cfa3b05b40e37ae9b70e43f31fcfa8eb98c6
Katherine Bello <143258387+GitHubKatherine@users.noreply.github.com>
2023-10-13 15:08:54 +0700

    Create 1-whose-move.js

---
+++ javascript/2023/8kyu/1-whose-move.js
@@ -0,0 +1,20 @@
+// Simple Fun #261: Whose Move  |  PUZZLES
+// https://www.codewars.com/kata/59126992f9f87fd31600009b/solutions/javascript
+
+function whoseMove(lastPlayer, win) {
+   if (lastPlayer === "black" && win === false) {
+    return "white"
+  } else if (lastPlayer === "black" && win === true) {
+    return "black"
+  } else if (lastPlayer === "white" && win === true) {
+    return "white"
+  } else if (lastPlayer === "white" && win === false) {
+    return 'black'
+  }
+}
+
+console.log(whoseMove("black",false) ) //"white")
+
+console.log(whoseMove("white",true) ) //"white")
+
+console.log(whoseMove("white",false) ) //"black")
