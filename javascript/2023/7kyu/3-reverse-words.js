a4c837df870bc4ccadc623a5a32db2c8d54cade0
Katherine Bello <143258387+GitHubKatherine@users.noreply.github.com>
2023-10-15 00:58:39 +0700

    Update 3-reverse-words.js

--- javascript/2023/7kyu/3-reverse-words.js
+++ javascript/2023/7kyu/3-reverse-words.js
@@ -1,10 +1,65 @@
 // Reverse words
 // https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
 
+// function reverseWords(str) {
+//   const words = str.split(' ')
+//   const preResult = words.map((word) => {
+//     const chars = word.split('')
+//     let reversedChars = ''
+//     const lastIdx = chars.length - 1
+//     for (let i = lastIdx; i >= 0; i--) {
+//       reversedChars += chars[i]
+//     }
+
+//     return reversedChars
+
+//   })
+//   return preResult.join(' ')
+// }
+
+// function reverseWords(str) {
+//   const words = str.split(' ')
+//   const preResult = words.map((word) => {
+//     const chars = word.split('')
+//     let reversedChars = '' // ehT
+//     for (let i = 0; i < chars.length; i++) {
+//       reversedChars = chars[i] + reversedChars
+//     }
+
+//     return reversedChars
+//   })
+//   return preResult.join(' ')
+// }
+
+
+// function reverseWords(str) {
+//   const words = str.split(' ')
+//   const preResult = words.map((word) => {
+//     const chars = word.split('')
+//     const reversedChars = chars.reduce((acc, char) => {
+//       return char + acc
+//     }, '')
+
+//     return reversedChars
+//   })
+//   return preResult.join(' ')
+// }
+
+/**
+ * @param str {string}
+ * @returns {string}
+ */
 function reverseWords(str) {
-  const strSplit = str.split(' ')
-  const chars = strSplit.map((char) => char.split("").reverse().join(""))
-  return [...chars].join(' ')
+  const words = str.split(' ')
+  const preResult = words.map((word) => {
+    const chars = word.split('')
+    const reversedChars = chars.reduceRight((acc, char) => {
+      return acc + char
+    }, '')
+
+    return reversedChars
+  })
+  return preResult.join(' ')
 }
 
 console.log(reverseWords('The quick brown fox jumps over the lazy dog.')) // 'ehT kciuq nworb xof spmuj revo eht yzal .god'
