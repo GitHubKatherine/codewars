49607bda7a89c5f92493f59ab3d1886800a4c235
Katherine Bello <143258387+GitHubKatherine@users.noreply.github.com>
2023-12-10 01:04:21 +0700

    Add new approach - using map

--- typescript/2023/6kyu/6-pyramid-array.ts
+++ typescript/2023/6kyu/6-pyramid-array.ts
@@ -1,17 +1,27 @@
 // ✨ Pyramid Array  |  ALGORITHMS
 // https://www.codewars.com/kata/515f51d438015969f7000013
 
-export function pyramid(n: number): Array<Array<Number>> {
+// export function pyramid(n: number): Array<Array<Number>> {
+//   let arr: number[][] = []
+
+//   for (let i = 1; i <= n; i++) {
+//     let arrOne: number[] = []
+//     for (let j = 1; j <= i; j++) {
+//       arrOne.push(1)
+//     }
+//     arr.push(arrOne)
+//   }
+//   return arr
+// }
+
+export function pyramid(n: number, m: number = 1): Array<Array<Number>> {
   let arr: number[][] = []
 
-  for (let i = 1; i <= n; i++) {
-    let arrOne: number[] = []
-    for (let j = 1; j <= i; j++) {
-      arrOne.push(1)
-    }
-    arr.push(arrOne)
-  }
-  return arr
+  return Array.from<number[]>({ length: n }).map((_, idx) => {
+    const o = idx + 1
+    // return m.toString().repeat(m).split('').map(charOne => Number(charOne))
+    return m.toString().repeat(o).split('').map(Number)
+  })
 }
 
 console.log(pyramid(0)) // []
