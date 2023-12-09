fa7f7c29c4b8f96b0fa4972099f9a40ee605b588
Katherine Bello <143258387+GitHubKatherine@users.noreply.github.com>
2023-12-10 01:26:18 +0700

    Add new approach of Create Phone Number

--- typescript/2023/6kyu/2-create-phone-number.ts
+++ typescript/2023/6kyu/2-create-phone-number.ts
@@ -2,11 +2,17 @@
 // https://www.codewars.com/kata/525f50e3b73515a6db000b83/typescript
 
 export function createPhoneNumber(numbers: number[]): string {
-  const firstThreeIdx = numbers.slice(0, 3).join('')
-  const addHyphen = numbers.slice(3).join('').split('')
-  addHyphen.splice(3, 0, '-')
-  return `(${firstThreeIdx}) ${addHyphen.join('')}`
+  return numbers.reduce((acc, num) => {
+    return acc.replace("x", String(num)) // (123) 456-7890
+  }, "(xxx) xxx-xxxx")
 }
+
+// export function createPhoneNumber(numbers: number[]): string {
+//   const firstThreeIdx = numbers.slice(0, 3).join('')
+//   const addHyphen = numbers.slice(3).join('').split('')
+//   addHyphen.splice(3, 0, '-')
+//   return `(${firstThreeIdx}) ${addHyphen.join('')}`
+// }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // "(123) 456-7890"
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) ) // "(111) 111-1111"
