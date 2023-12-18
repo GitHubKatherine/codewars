ede9ec3161e66c1db376018ff9dda810e44167ad
Katherine Bello <143258387+GitHubKatherine@users.noreply.github.com>
2023-12-18 18:01:47 +0700

    Update 17-the-falling-speed-of-petals.ts

--- typescript/2023/8kyu/17-the-falling-speed-of-petals.ts
+++ typescript/2023/8kyu/17-the-falling-speed-of-petals.ts
@@ -5,8 +5,8 @@ export function sakuraFall(v: number): number {
     return v <= 0 ? 0 : (80 *5) / v
   }
 
-console.log(sakuraFall(5)) //  80 // 1e-9
-console.log(sakuraFall(10)) //  40 // 1e-9
-console.log(sakuraFall(200)) //  2 // 1e-9
-console.log(sakuraFall(-1)) //  0 // 1e-9
-console.log(sakuraFall(0)) //  0 // 1e-9
+console.log(sakuraFall(5)) //  80 
+console.log(sakuraFall(10)) //  40
+console.log(sakuraFall(200)) //  2
+console.log(sakuraFall(-1)) //  0
+console.log(sakuraFall(0)) //  0
